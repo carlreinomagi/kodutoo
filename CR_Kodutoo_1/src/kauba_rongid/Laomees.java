0b16fdc01316b946bc0389fd6087ec0a4022693c
Reaaally <reaaally@gmail.com>
2014-11-19 14:10:31 +0200

    Esimene kodutöö

---
+++ CR_Kodutoo_1/src/kauba_rongid/Laomees.java
@@ -0,0 +1,30 @@
+package kauba_rongid;
+
+public class Laomees {
+
+	public static void alusteKogusRongis(Rong[] aluseid) {
+		for( int i = 0; i < aluseid.length; i++){
+			System.out.println(i+1+". "+aluseid[i]);
+		}
+		System.out.println("_____________________________________;");
+	}
+
+	public static void main(String[] args) {
+		// Lisatakse juurde kaks rongi, ühes on 9 alust ja teises 25 alust.
+		Rong[] rongid = new Rong[2];
+		rongid[0] = new Rong(9);
+		rongid[1] = new Rong(25);
+		
+		// Kui palju aluseid rongis on
+		alusteKogusRongis(rongid);
+		// ühest orngist võetakse alus ja lisatakse teise. (tõstetakse ümber)
+		rongid[0].eemaldaAlus();
+		rongid[1].lisaAlus();
+		
+		// Rongis olevad alused loetakse uuesti üle.
+		for(int i = 0; i < rongid.length; i++){
+			System.out.println(i+1+". rongis on "+rongid[i].kysiAlusteKogus()+" alust");
+		}
+	}
+	
+}
---
+++ CR_Kodutoo_1/src/kauba_rongid/Rong.java
@@ -0,0 +1,53 @@
+package kauba_rongid;
+
+public class Rong {
+	int AluseidRongis = 1;
+	int minAluseidRongis = 0;
+	int maxAluseidRongis = 25;
+	
+	public void lisaAlus(){
+		if(AluseidRongis < maxAluseidRongis){
+			System.out.println("Rongis oli"+AluseidRongis+" alust");
+			AluseidRongis++;
+			System.out.println("Rongis on"+AluseidRongis+" alust");
+		}
+		else{
+			System.out.println("Vagunid on aluseid täis");
+		}
+		System.out.println("_____________________________________!");
+	}
+	
+	public void eemaldaAlus(){
+		if(AluseidRongis > minAluseidRongis){
+			System.out.println("Rongis oli"+AluseidRongis+" alust");
+			AluseidRongis--;
+			System.out.println("Rongis on"+AluseidRongis+" alust");
+		}
+		else{
+			System.out.println("Aluseid vagunites ei ole");
+		}
+	System.out.println("_____________________________________?");
+	}
+	
+	public int kysiAlusteKogus(){
+		return AluseidRongis;
+	}
+	
+	public Rong(){
+		this(0);
+	}
+	public Rong(int algseltAluseidRongis){
+		if(algseltAluseidRongis>maxAluseidRongis){
+			algseltAluseidRongis = maxAluseidRongis;
+		}
+		if(algseltAluseidRongis<maxAluseidRongis){
+			algseltAluseidRongis = minAluseidRongis;
+		}
+		AluseidRongis = algseltAluseidRongis;
+	}
+	//väljastus inimkeeles.
+	public String toString(){
+		return "Rongis on "+AluseidRongis+" alust";
+	}
+
+}
