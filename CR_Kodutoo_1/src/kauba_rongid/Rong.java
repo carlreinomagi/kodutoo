a72774c62af2896aff3da9c558a8928133a775ca
Reaaally <reaaally@gmail.com>
2014-11-19 14:12:25 +0200

    Parandused

--- CR_Kodutoo_1/src/kauba_rongid/Rong.java
+++ CR_Kodutoo_1/src/kauba_rongid/Rong.java
@@ -14,7 +14,7 @@ public class Rong {
 		else{
 			System.out.println("Vagunid on aluseid täis");
 		}
-		System.out.println("_____________________________________!");
+		System.out.println("______________________________________!");
 	}
 	
 	public void eemaldaAlus(){
